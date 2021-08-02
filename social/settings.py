a972426493c6e93bfe8a58cc7ed4c789e8243d47
mhdshjn <mohammedshajin1997@gmail.com>
2021-08-02 16:01:18 +0530

    user auth

--- social/settings.py
+++ social/settings.py
@@ -15,6 +15,10 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+LOGIN_URL = 'home'
+LOGIN_REDIRECT_URL='home'
+LOGOUT_REDIRECT_URL='home'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
