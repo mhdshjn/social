83068468b505779c3e93ca222e5f3a944fc9a2bc
mhdshjn <mohammedshajin1997@gmail.com>
2021-08-04 12:50:13 +0530

    fixed errors

--- core/views.py
+++ core/views.py
@@ -7,13 +7,14 @@ def home(request):
     return render(request, "home.html")
 
 def signup(request):
+    form = UserCreationForm()
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request,user)
-            return redirect("home")
-    else:
-        form= UserCreationForm()
-    
+            login(request, user)
+        else:
+            form= UserCreationForm()
+        return redirect("home")
+
     return render(request, "signup.html",{'form':form})
